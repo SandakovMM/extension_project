1c1691d6c221b4d4942602588caa212f5a7c6371
SandakovMM <G0odvinSun@gmail.com>
2014-07-09 18:14:33 +0700

    Add castom button. Check how it's work.

---
+++ friendly-logs/plib/hooks/CustomButtons.php
@@ -0,0 +1,16 @@
+<?php
+
+class Modules_CustomButtons_CustomButtons extends pm_Hook_CustomButtons
+{
+
+    public function getButtons() // Functions must return list of list with our buttons properties
+    {
+        return [[
+            'place' => self::PLACE_DOMAIN_PROPERTIES,
+            'title' => 'Friendly logs',
+            'description' => 'Button to call friendly logs extension',
+            'link' => pm_Context::getActionUrl('form'),
+        ]];
+    }
+
+}
